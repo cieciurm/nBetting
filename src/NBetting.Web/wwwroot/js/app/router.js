3b7d99224759e46109134c3d9282219c230ae594
Jaroslaw Adamczyk <jaradam1992@gmail.com>
2016-09-23 19:17:15 +0200

    Turn on html5mode

--- src/NBetting.Web/wwwroot/js/app/router.js
+++ src/NBetting.Web/wwwroot/js/app/router.js
@@ -2,6 +2,7 @@
 var homeController = require('./home/homeController.js');
 
 function Router($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
         .when("/home", {
             templateUrl: 'views/Home/index.html',
